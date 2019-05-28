e39f70940fd46af055f6dcaad55469564a9faf8a
Unknown <maxvnoordennen@gmail.com>
2019-05-28 13:44:22 +0200

    [Added] Win screen
    TODO: Animations for both screens

--- AR-Applicatie/AR-Applicatie/states/WinState.cpp
+++ AR-Applicatie/AR-Applicatie/states/WinState.cpp
@@ -1,4 +1,7 @@
 #include "WinState.h"
+#include <GL/freeglut.h>
+#include "../util/TextureHandler.h"
+#include "../data/DataManager.h"
 
 WinState::WinState() = default;
 
@@ -6,6 +9,8 @@ WinState::~WinState() = default;
 
 void WinState::draw(std::map<std::string, Graphics::mesh>& meshes, std::map<std::string, uint16_t>& textures)
 {
+	DataManager::getInstance().drawBackgroundScreen();
+	DataManager::getInstance().drawDefaultText(500, 390, "You win", GLUT_STROKE_ROMAN);
 }
 
 void WinState::update(float elapsedTime)
